59bcca2da3d8ec6da3eef90e661f90c731ddf245
Michael duPont <michael@mdupont.com>
2017-08-29 22:52:26 -0400

    Add required parameters that are not yet used

--- open511/api/validators.py
+++ open511/api/validators.py
@@ -6,3 +6,5 @@ from flask_restful.reqparse import RequestParser
 
 BASE_PARSER = RequestParser()
 BASE_PARSER.add_argument('format', type=str, help='json (default) or xml')
+BASE_PARSER.add_argument('version', type=str, help='Only supports "1.0"')
+BASE_PARSER.add_argument('accept-language', type=str, help='Only supports "en"')
